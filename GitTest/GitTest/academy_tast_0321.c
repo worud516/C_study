2e539e3fa6e34f0b55ce83ecfaa17fa6d523a9c7
worud516 <worud516@naver.com>
2021-03-21 20:24:06 +0900

    academy_task_0321

---
+++ GitTest/GitTest/academy_tast_0321.c
@@ -0,0 +1,24 @@
+// 문제 : 입력받은 2가지 정수 사이에 존재하는 3의 배수 중에서 홀수의 합을 리턴하는 함수 구현
+
+#include <stdio.h>
+
+int get_sum(int a, int b) {
+
+	int sum = 0;
+
+	for (int i = a; i <= b; i++) {
+		if (i % 3 == 0 && i % 2 == 1) {
+			sum += i;
+		}
+	}
+	return sum;
+}
+
+
+
+int main(void) {
+	
+	printf("결과 : %d\n", get_sum(50, 100));
+	
+	return 0;
+}
